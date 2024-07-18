473d3fdbf2431295385638ad096de6ead04beb66
luxem97 <markluxem18@gmail.com>
2024-07-19 00:54:02 +0200

    siehe voheriger commit

--- src/main.js
+++ src/main.js
@@ -20,23 +20,21 @@ let start = () => {
 $(".plus10").click(function(){
     console.log("plus10 clicked");
     givePoints(10);
-    update();
 });
 
 $(".plus20").click(function(){
     console.log("plus20 clicked");
     givePoints(20);
-    update();
 });
     
 let givePoints = (points) => {
-  
     if(player_1.turn){
         player_1.score += points;
     }
     if(player_2.turn){
         player_2.score += points;
     }
+    update();
 }
 
 let update = () => { 
@@ -47,19 +45,21 @@ let update = () => {
 };
 
 let erzeugeSpielfeld = () => {
-//Bullseye,
-//Bull,
-//20 x 1x Felder, 
-//20 x 3x Felder,
-//weitere 20 x 1x Felder
-//20 x 2x Felder
-document.getElementById("spielfeld").innerHTML = "<div class='bullseye'>"+returnSvgString(50, 399, false)+"</div>"
+    //Bullseye,
+    //Bull,
+    //20 x 1x Felder, 
+    //20 x 3x Felder,
+    //weitere 20 x 1x Felder
+    //20 x 2x Felder
+    document.getElementById("spielfeld").innerHTML = "<div class='bullseye_div'>"+returnSvgString(100, 100, "bullseye", true)+"</div>"
+    document.getElementsByClassName("bullseye")[0].addEventListener("click", function(){
+        givePoints(50);
+    });
 }
-
-let returnSvgString = (height, width, circle) => {
+let returnSvgString = (height, width, classname, circle) => {
     if(circle){
-        return "<svg width='"+width+"' height='"+height+"'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red' /></svg>";
+        return "<svg width='"+width+"' height='"+height+"'><circle class='"+classname+"' cx='"+width/2+"' cy='"+height/2+"' r='40' stroke='black' stroke-width='3' fill='red' /></svg>";
     }else{
-        return "<svg width='"+width+"' height='"+height+"'><polygon points='0, 0, "+width+", 0, "+width/2+", "+height+"' fill='yellow' /></svg>"
+        return "<svg width='"+width+"' height='"+height+"'><polygon class='"+classname+"' points='0, 0, "+width+", 0, "+width/2+", "+height+"' fill='yellow' /></svg>"
     }
 }
